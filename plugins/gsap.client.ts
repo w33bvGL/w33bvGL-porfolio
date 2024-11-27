719216cb79cbf0ae4c553c6f90d0aaa296219771
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-11-27 19:24:08 +0400

    remove vue format

--- plugins/gsap.client.ts
+++
@@ -1,9 +0,0 @@
-import { gsap } from '~/assets/js/gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollSmoother} from "gsap/ScrollSmoother";
-
-export default defineNuxtPlugin((nuxtApp) => {
-    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
-
-    nuxtApp.provide('gsap', gsap);
-});
