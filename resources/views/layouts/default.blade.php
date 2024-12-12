3a0e63b8df1827fffdd912944b75c29a151222c4
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-12-12 11:12:18 +0400

    add fonts

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,23 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Laravel App')</title>
+
+    @vite(['resources/css/app.scss', 'resources/scss/app.scss'])
 </head>
 <body>
-<header>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-    </nav>
-</header>
 
 <main>
     @yield('content')
 </main>
 
-<footer>
-    <p>&copy; {{ date('Y') }} My Laravel App</p>
-</footer>
+<!-- CURSOR START -->
+<div class="rts-cursor cursor-outer" data-default="yes" data-link="yes" data-slider="no">
+    <span class="fn-cursor"></span>
+</div>
+<div class="rts-cursor cursor-inner" data-default="yes" data-link="yes" data-slider="no">
+        <span class="fn-cursor">
+        <span class="fn-left"></span>
+        <span class="fn-right"></span>
+        </span>
+</div>
+<!-- CURSOR END -->
+
+<!-- THEME SWITCHER START-->
+<div class="modal-sidebar-scroll rts-dark-light">
+    <ul>
+        <li class="go-dark-w"><span>Dark</span><i class="rts-go-dark fal fa-moon"></i></li>
+        <li class="go-light-w"><span>Light</span><i class="rts-go-light fa-light fa-brightness"></i></li>
+    </ul>
+</div>
+<!-- THEME SWITCHER END -->
 
 </body>
 </html>
