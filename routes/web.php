905344fbaa481a93a85d56b8d4771e9f774996c2
w33bvGL <140258231+w33bvGL@users.noreply.github.com>
2025-01-23 20:41:18 +0000

    Apply automatic changes

--- routes/web.php
+++ routes/web.php
@@ -7,18 +7,18 @@ use Illuminate\Support\Facades\Route;
 Route::group(
     [
         'prefix' => LaravelLocalization::setLocale(),
-        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'localeCookieRedirect' ]
-    ], function(){
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath', 'localeCookieRedirect'],
+    ], function () {
 
-    Route::view('/', 'welcome');
+        Route::view('/', 'welcome');
 
-    Route::view('dashboard', 'dashboard')
-        ->middleware(['auth', 'verified'])
-        ->name('dashboard');
+        Route::view('dashboard', 'dashboard')
+            ->middleware(['auth', 'verified'])
+            ->name('dashboard');
 
-    Route::view('profile', 'profile')
-        ->middleware(['auth'])
-        ->name('profile');
-});
+        Route::view('profile', 'profile')
+            ->middleware(['auth'])
+            ->name('profile');
+    });
 
 require __DIR__.'/auth.php';
