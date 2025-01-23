f1ba8595b4959d05cde383e9be618d11cc649379
w33bvGL <w33bv.gl@gmail.com>
2025-01-23 21:41:11 +0300

    remove schema json its not found

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'vahesargsyan') }} + Vahe Sargsyan - Full-Stack Engineer | Portfolio & Projects</title>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="">
+    <body>
 
     </body>
 </html>
