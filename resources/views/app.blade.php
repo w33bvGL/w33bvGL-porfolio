68c8a01ae0d3dd7edd26d0a0b68ed274fab7879d
w33bvGL <w33bv.gl@gmail.com>
2025-01-24 12:27:42 +0300

    add bootsrap files

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,12 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }}</title>
+    <title>{{ __('title.title', ['APP_NAME' => config('app.name')] ) }}</title>
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="font-sans antialiased">
 
-<div id="app" class="min-h-screen dark:bg-red-200">
+@include('components.common.cursor')
+
+<div id="app" class="min-h-screen bg-black">
     @yield('content')
 </div>
 
