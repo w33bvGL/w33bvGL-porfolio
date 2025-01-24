eddb3c825fc8c5134e4d6f3250e0a90e098b8fee
w33bvGL <w33bv.gl@gmail.com>
2025-01-24 11:38:54 +0300

    remove unused blades

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
 </head>
 <body class="font-sans antialiased">
 
-<div id="app" class="min-h-screen dark:bg-gray-900 bg-gray-100 ">
+<div id="app" class="min-h-screen dark:bg-red-200">
     @yield('content')
 </div>
 
