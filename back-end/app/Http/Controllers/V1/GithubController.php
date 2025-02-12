159c12e69b1c1509ad98b8d9cf770c9f673c0e5a
w33bvGL <w33bv.gl@gmail.com>
2025-02-12 15:19:18 +0400

    add controller error files

--- back-end/app/Http/Controllers/V1/GithubController.php
+++ back-end/app/Http/Controllers/V1/GithubController.php
@@ -6,23 +6,23 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
-class GitHubController extends Controller
+class GithubController extends Controller
 {
-    public function getGitHubProfile(Request $request): JsonResponse
+    public function getGitHubProfile(): JsonResponse
     {
-        $username = congig('');
+        $baseUrl      = config('github.base_url');
+        $username = config('github.github_username');
 
-        $response = Http::get('https://api.github.com/users/w33bvGL');
+        $response = Http::get($baseUrl . "/users/" . $username);
 
         if ($response->successful()) {
             return response()->json($response->json());
         }
 
         return response()->json([
-            'error' => 'Не удалось получить данные с GitHub',
+            'error' => __('githubController.failed_to_fetch_data'),
         ], 500);
     }
 }
---
+++ back-end/resources/lang/ru/githubController.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'failed_to_fetch_data' => 'Не удалось получить данные с GitHub',
+];
