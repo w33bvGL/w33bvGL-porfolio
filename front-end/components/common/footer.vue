cbc7dc71ac5f492e944d1999c7ff1cd8eef8848f
w33bvGL <w33bv.gl@gmail.com>
2025-02-12 14:45:03 +0400

    add ts scripts

--- front-end/components/common/footer.vue
+++ front-end/components/common/footer.vue
@@ -1,10 +1,35 @@
 <script setup lang="ts">
 import { useI18n } from "#i18n";
-
 const { t, locale } = useI18n();
 
-const { data } = await useAsyncData("footer-message", () =>
+interface FooterMessageData {
+  id: number;
+  slug: string;
+  translations: {
+    [key: string]: string;
+  };
+}
+
+const { data: footerMessageData } = await useAsyncData<FooterMessageData>("messages-random", () =>
     $fetch("/api/footer/messages/random", {
+      method: 'GET',
+      headers: { 'Accept-Language': locale.value },
+    })
+);
+
+interface QuoteData {
+  id: number;
+  slug: string;
+  translations: {
+    [key: string]: {
+      author: string;
+      quote: string;
+    };
+  };
+}
+
+const { data: footerQuoteData } = await useAsyncData<QuoteData>("quote-random", () =>
+    $fetch("/api/quote/random", {
       method: 'GET',
       headers: { 'Accept-Language': locale.value },
     })
@@ -14,13 +39,13 @@ const { data } = await useAsyncData("footer-message", () =>
 <template>
   <div class="my-6">
     <div class="flex justify-between items-center">
-      <p class="text-sm">&copy; 2022 - {{ new Date().getFullYear() }} Vahe Sargsyan. {{ data?.translations[locale.valueOf()] || data?.translations.en }}</p>
+      <p class="text-sm">
+        &copy; 2022 - {{ new Date().getFullYear() }} Vahe Sargsyan.
+        {{ footerMessageData?.translations[locale.valueOf()] || footerMessageData?.translations.en }}
+      </p>
       <div class="italic text-sm text-gray-500">
-<!--        {{ data?.translations[locale.valueOf()] || data?.translations.en }}-->
+        <p>{{ footerQuoteData?.translations[locale.valueOf()]?.quote || footerQuoteData?.translations.en?.quote }} - {{ footerQuoteData?.translations[locale.valueOf()]?.author || footerQuoteData?.translations.en?.author }}</p>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-</style>
