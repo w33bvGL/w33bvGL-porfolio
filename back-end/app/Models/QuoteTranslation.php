ebae1b17785e54fc4d50d8531671a6b99d5a7136
w33bvGL <w33bv.gl@gmail.com>
2025-02-16 19:17:51 +0400

    remove unuesed files

---
+++ back-end/app/Models/QuoteTranslation.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @property string $author
+ * @property string $quote
+ */
+class QuoteTranslation extends Model
+{
+    /**
+     * @var list<string>
+     */
+    protected $fillable = [
+        'quote',
+        'author',
+    ];
+
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'quote' => 'string',
+        'author' => 'string',
+    ];
+
+    /** @return BelongsTo<Quote, $this> */
+    public function quote(): BelongsTo
+    {
+        return $this->belongsTo(Quote::class, 'quote_id');
+    }
+
+    /** @return BelongsTo<Language, $this> */
+    public function language(): BelongsTo
+    {
+        return $this->belongsTo(Language::class, 'language_id');
+    }
+}
