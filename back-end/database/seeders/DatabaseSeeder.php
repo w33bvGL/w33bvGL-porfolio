9667936852d0623cf062b89aa4eb74a2331fe492
w33bvGL <w33bv.gl@gmail.com>
2025-02-17 16:30:45 +0400

    add migrations and seeds

---
+++ back-end/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        $this->call([
+            LanguageSeeder::class,
+            SocialSeeder::class,
+            FooterMessageSeeder::class,
+            QuoteSeeder::class,
+        ]);
+    }
+}
