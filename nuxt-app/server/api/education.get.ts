64057742b014b5773073e80951ca01f31dc76fdd
Vahe Sargsyan <w33bv.gl@gmail.com>
2025-06-07 18:38:26 +0400

    add expirience

--- nuxt-app/server/api/education.get.ts
+++ nuxt-app/server/api/education.get.ts
@@ -1,63 +1,58 @@
 export default defineEventHandler(() => {
   return {
     en: [
-        {
-          title: 'Software engineering',
-          description: 'Studied from 2020 to 2024'
-        },
-        {
-          title: 'Friend LLC - Software engineer',
-          description: 'Worked from 2022 to 2023'
-        },
-        {
-          title: 'DevOps Internship at InoRain LLC',
-          description: 'Interned from 2021 to 2022'
-        },
-        {
-          title: 'Software engineer, Ararat State College',
-          description: 'Graduated with a focus on software development'
-        }
-      ],
+      {
+        title: 'Software engineering',
+        description: 'Studied from 2020 to 2024'
+      },
+      {
+        title: 'Friend LLC - Software engineer',
+        description: 'Worked from 2022 to 2023'
+      },
+      {
+        title: 'DevOps Internship at InoRain LLC',
+        description: 'Interned from 2021 to 2022'
+      },
+      {
+        title: 'Software engineer, Ararat State College',
+        description: 'Graduated with a focus on software development'
+      }
     ],
-
     ru: [
-        {
-          title: 'Инженер программного обеспечения',
-          description: 'Учился с 2020 по 2024 год'
-        },
-        {
-          title: 'Friend LLC - инженер-программист',
-          description: 'Работал с 2022 по 2023 год'
-        },
-        {
-          title: 'Стажировка по DevOps в InoRain LLC',
-          description: 'Проходил стажировку с 2021 по 2022 год'
-        },
-        {
-          title: 'Программист, Араратский государственный колледж',
-          description: 'Окончил с уклоном в разработку ПО'
-        }
-      ],
+      {
+        title: 'Инженер программного обеспечения',
+        description: 'Учился с 2020 по 2024 год'
+      },
+      {
+        title: 'Friend LLC - инженер-программист',
+        description: 'Работал с 2022 по 2023 год'
+      },
+      {
+        title: 'Стажировка по DevOps в InoRain LLC',
+        description: 'Проходил стажировку с 2021 по 2022 год'
+      },
+      {
+        title: 'Программист, Араратский государственный колледж',
+        description: 'Окончил с уклоном в разработку ПО'
+      }
     ],
-
     hy: [
-        {
-          title: 'Ծրագրավորման ինժեներ',
-          description: 'Սովորել եմ 2020-ից մինչև 2024 թվականը'
-        },
-        {
-          title: 'Friend LLC - Ծրագրավորման ինժեներ',
-          description: 'Աշխատել եմ 2022-ից մինչև 2023 թվականը'
-        },
-        {
-          title: 'DevOps պրակտիկա InoRain LLC-ում',
-          description: 'Անցել եմ պրակտիկա 2021-ից մինչև 2022 թվականը'
-        },
-        {
-          title: 'Ծրագրավորող, Արարատ պետական քոլեջ',
-          description: 'Շրջանավարտ՝ ծրագրավորման ուղղվածությամբ'
-        }
-      ],
-      ]
+      {
+        title: 'Ծրագրավորման ինժեներ',
+        description: 'Սովորել եմ 2020-ից մինչև 2024 թվականը'
+      },
+      {
+        title: 'Friend LLC - Ծրագրավորման ինժեներ',
+        description: 'Աշխատել եմ 2022-ից մինչև 2023 թվականը'
+      },
+      {
+        title: 'DevOps պրակտիկա InoRain LLC-ում',
+        description: 'Անցել եմ պրակտիկա 2021-ից մինչև 2022 թվականը'
+      },
+      {
+        title: 'Ծրագրավորող, Արարատ պետական քոլեջ',
+        description: 'Շրջանավարտ՝ ծրագրավորման ուղղվածությամբ'
+      }
+    ]
   }
 })
