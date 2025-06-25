8f7df832e277d6a391d9f1c6d78820924bf8c1a4
Vahe Sargsyan <w33bv.gl@gmail.com>
2025-06-25 09:41:07 +0400

    Fix GithubLinkButton position for non-lg screens
    The -translate-x-19 class is now applied only on large screens (lg), improving the button's positioning on smaller devices.

--- app/components/common/GithubLinkButton.vue
+++ app/components/common/GithubLinkButton.vue
@@ -3,7 +3,7 @@ const { global } = useAppConfig()
 </script>
 
 <template>
-  <div class="absolute top-0 -translate-x-19 fixed pt-4">
+  <div class="absolute top-0 lg:-translate-x-19 fixed pt-4">
     <UTooltip text="Open in Github">
       <UButton icon="i-simple-icons-github" color="neutral" target="_blank" :to="global.projectRepository" />
     </UTooltip>
