1610f279650ef0a851bd1c8fb5f8db42437f747c
Vahe Sargsyan <w33bv.gl@gmail.com>
2025-06-25 09:32:27 +0400

    Refactor GitHub link button into separate component
    Extracted the GitHub link button from the default layout into a new CommonGithubLinkButton component for better reusability and cleaner layout code.

---
+++ app/components/common/GithubLinkButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="absolute top-0 -translate-x-19 fixed pt-4">
+    <UTooltip text="Open in Github">
+      <UButton icon="i-simple-icons-github" color="neutral" target="_blank" to="https://github.com/w33bvGL/w33bvGL-porfolio" />
+    </UTooltip>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,11 +9,7 @@
         <slot />
       </div>
       <AppFooter />
-      <div class="absolute top-0 -translate-x-19 fixed pt-4">
-        <UTooltip text="Open in Github">
-          <UButton icon="i-simple-icons-github" color="neutral" target="_blank" to="https://github.com/w33bvGL/w33bvGL-porfolio" />
-        </UTooltip>
-      </div>
+      <CommonGithubLinkButton/>
     </UContainer>
   </div>
 </template>
