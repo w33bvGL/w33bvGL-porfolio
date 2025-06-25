fcbb5cb2fa56d1788c6eef02b873d07455ba9df0
Vahe Sargsyan <w33bv.gl@gmail.com>
2025-06-25 09:31:13 +0400

    Add GitHub link button to default layout
    Introduces a fixed button with a GitHub icon and tooltip in the default layout, linking to the project's GitHub repository for easier access.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,12 +3,17 @@
 
 <template>
   <div>
-    <UContainer class="sm:border-x border-default max-w-4xl sm:pt-10">
+    <UContainer class="sm:border-x border-default max-w-4xl sm:pt-10 relative">
       <AppHeader />
       <div class="py-10">
         <slot />
       </div>
       <AppFooter />
+      <div class="absolute top-0 -translate-x-19 fixed pt-4">
+        <UTooltip text="Open in Github">
+          <UButton icon="i-simple-icons-github" color="neutral" target="_blank" to="https://github.com/w33bvGL/w33bvGL-porfolio" />
+        </UTooltip>
+      </div>
     </UContainer>
   </div>
 </template>
